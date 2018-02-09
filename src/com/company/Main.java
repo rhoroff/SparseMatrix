477d3994f73bd01f8741bb90d4276efe81164202
Ryan Horoff <rhoroff@gmail.com>
2018-02-09 12:59:41 -0500

    Overwritten cases of addElement()
    Added overwritten conditions for the addElement method().
    Remove element methods still need doing.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,29 @@
 package com.company;
 
-public static void main(String[] args){
+public class Main{
+    public static void main(String[] args){
+        SparseMatrix testMatrix = new SparseMatrix();
+        /*The Matrix is empty*/
+        testMatrix.setSize(3);
+        testMatrix.addElement(0,0,1);
+        testMatrix.addElement(0,1,4);
+        testMatrix.addElement(2,2,5);
+        testMatrix.addElement(1,1,3);
+        testMatrix.addElement(1,1,9);
+        testMatrix.addElement(0,0,5);
+        System.out.println(testMatrix.toString());
+        System.out.println(testMatrix.getElement(0,0));
+        System.out.println(testMatrix.getElement(0,1));
+        System.out.println(testMatrix.getElement(1,1));
+        System.out.println(testMatrix.getElement(2,2));
 
-        }
+
+
+
+
+
+
+
+
+    }
+}
